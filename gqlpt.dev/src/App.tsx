e7ca894b768f55bf69405dfa5b117a825be10f30
daniel starns <danielstarns@hotmail.com>
2023-02-07 16:50:06 +0000

    client: *

--- gqlpt.dev/src/App.tsx
+++ gqlpt.dev/src/App.tsx
@@ -72,7 +72,7 @@ function App() {
   return (
     <div>
       <div className="container mx-auto bg-gray-100 rounded-xl shadow border p-8 m-10">
-        <h1 className="text-3xl text-gray-700 font-bold mb-5">GQLTP</h1>
+        <h1 className="text-3xl text-gray-700 font-bold mb-5">GQLPT</h1>
         <p className="text-gray-500 text-lg mt-5">
           GQLPT leverages the power of ChatGPT to generate GraphQL queries from
           plain text.
