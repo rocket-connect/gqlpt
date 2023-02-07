2c9630222b57a581461564364ecf750656aa9962
daniel starns <danielstarns@hotmail.com>
2023-02-07 18:00:29 +0000

    fix: change default queries

--- gqlpt.dev/src/defaults.ts
+++ gqlpt.dev/src/defaults.ts
@@ -2,25 +2,25 @@ import { print, parse } from "graphql";
 
 export const typeDefs = print(
   parse(`
-    type User {
-        name: String
-        email: String
-        posts(where: PostWhere): [Post]
-    }
-
-    type Post {
-        id: ID
-        name: String
-    }
-
-    input PostWhere {
-        title_contains: String
-    }
-
-    type Query {
-        users: [User]
-    }
-`)
+        type User {
+            name: String
+            email: String
+            posts: [Post]
+        }
+        
+        type Post {
+            id: ID
+            name: String
+        }
+        
+        input UserWhere {
+            name: String
+        }
+        
+        type Query {
+            users(where: UserWhere): [User]
+        }
+    `)
 );
 
-export const query = `find users including posts`;
+export const query = `find user dan and his posts`;
