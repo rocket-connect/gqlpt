99bfb4efcb56328336467d2a69617e0781f55d91
danstarns <danielstarns@hotmail.com>
2023-09-25 22:06:16 +0100

    webpack: remove terser

--- apps/playground/webpack.config.cjs
+++ apps/playground/webpack.config.cjs
@@ -2,7 +2,6 @@ const path = require("path");
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
-const TerserPlugin = require("terser-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
 
@@ -14,14 +13,6 @@ module.exports = {
   resolve: {
     extensions: [".ts", ".tsx", ".mjs", ".json", ".js"],
   },
-  ...(process.env.NODE_ENV === "production"
-    ? {
-        optimization: {
-          minimize: true,
-          minimizer: [new TerserPlugin()],
-        },
-      }
-    : {}),
   module: {
     rules: [
       {
