65f0e415e06807d76690e5ecd4eabaa78bd2e5f7
danstarns <danielstarns@hotmail.com>
2024-10-08 19:13:10 -0700

    remove docusaurus

--- apps/docs/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/danielstarns/code/rocket-connect/gqlpt/node_modules/.pnpm/infima@0.2.0-alpha.44/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/danielstarns/code/rocket-connect/gqlpt/node_modules/.pnpm/@docusaurus+theme-classic@3.5.2_@types+react@18.3.3_eslint@8.57.0_react-dom@18.2.0_react@18.2_xs7ja5p4zcgr5pnye24adhpdhq/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/danielstarns/code/rocket-connect/gqlpt/node_modules/.pnpm/@docusaurus+theme-classic@3.5.2_@types+react@18.3.3_eslint@8.57.0_react-dom@18.2.0_react@18.2_xs7ja5p4zcgr5pnye24adhpdhq/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/danielstarns/code/rocket-connect/gqlpt/apps/docs/src/css/custom.css"),
-];
