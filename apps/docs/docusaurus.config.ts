18af9c9b672ad63996d1717d4dcc381d43407e67
danstarns <danielstarns@hotmail.com>
2024-11-02 14:04:05 +0000

    feat: add query retry

--- apps/docs/docusaurus.config.ts
+++ apps/docs/docusaurus.config.ts
@@ -23,7 +23,7 @@ const config: Config = {
     locales: ["en"],
   },
   customFields: {
-    API_URL: process.env.API_URL || "",
+    API_URL: process.env.API_URL || "http://localhost:5000",
   },
   plugins: [tailwindPlugin],
   presets: [
--- packages/adapter-base/src/index.ts
+++ packages/adapter-base/src/index.ts
@@ -1,5 +1,12 @@
+export interface AdapterResponse {
+  content: string;
+  conversationId?: string;
+}
+
 export abstract class Adapter {
   abstract connect(): Promise<void>;
-
-  abstract sendText(text: string): Promise<string>;
+  abstract sendText(
+    text: string,
+    conversationId?: string,
+  ): Promise<AdapterResponse>;
 }
